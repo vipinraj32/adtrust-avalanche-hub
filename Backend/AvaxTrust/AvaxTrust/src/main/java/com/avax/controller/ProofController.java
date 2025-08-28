717260904087de14593ad7bbb2dad33b32582210
vipin singh <vipendrarajpoot2@gmail.com>
2025-08-29 01:18:08 +0530

    add ProofController

---
+++ Backend/AvaxTrust/AvaxTrust/src/main/java/com/avax/controller/ProofController.java
@@ -0,0 +1,25 @@
+package com.avax.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.avax.bean.Proof;
+import com.avax.service.ProofService;
+
+@RestController
+@RequestMapping("/api/proofs")
+public class ProofController {
+
+    @Autowired
+    private  ProofService proofService;
+
+    @GetMapping("/add")
+    public ResponseEntity<Proof> addProof(@RequestBody Proof proof){
+        Proof createdProof = proofService.addProof(proof);
+        return ResponseEntity.ok(createdProof);
+    }
+    
+}
